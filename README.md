6e45f83d1d7d58bd6cf7e2a7d3f189071c4d82bf
Khánh Duy <150520249+DuyNTK3020@users.noreply.github.com>
2024-11-09 02:00:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chess
+Source Code
+# Chess
---
+++ game.cpp
@@ -0,0 +1,261 @@
+#include "game.h"
+#include "button.h"
+#include <QPixmap>
+#include "king.h"
+#include <QDebug>
+#include <QLineEdit>
+#include <QGraphicsProxyWidget>
+Game::Game(QWidget *parent ):QGraphicsView(parent)
+{
+
+    //Making the Scene
+    gameScene = new QGraphicsScene();
+    gameScene->setSceneRect(0,0,1400,900);
+
+    //Making the view
+    setFixedSize(1400,900);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setScene(gameScene);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::black);
+    setBackgroundBrush(brush);
+    pieceToMove = NULL;
+
+    //display turn
+    turnDisplay = new QGraphicsTextItem();
+    turnDisplay->setPos(width()/2-100,10);
+    turnDisplay->setZValue(1);
+    turnDisplay->setDefaultTextColor(Qt::white);
+    turnDisplay->setFont(QFont("",18));
+    turnDisplay->setPlainText("Turn : WHITE");
+
+    //display Check
+    check = new QGraphicsTextItem();
+    check->setPos(width()/2-100,860);
+    check->setZValue(4);
+    check->setDefaultTextColor(Qt::red);
+    check->setFont(QFont("",18));
+    check->setPlainText("CHECK!");
+    check->setVisible(false);
+    setTurn("WHITE");
+
+}
+
+void Game::drawChessBoard()
+{
+    chess = new ChessBoard();
+    drawDeadHolder(0,0,Qt::lightGray);
+    drawDeadHolder(1100,0,Qt::lightGray);
+     chess->drawBoxes(width()/2-400,50);
+
+}
+
+void Game::displayDeadWhite()
+{
+    int SHIFT = 50;
+    int j = 0;
+    int k = 0;
+    for(size_t i = 0,n = whiteDead.size(); i<n; i++) {
+            if(j == 4){
+                k++;
+                j = 0;
+            }
+            whiteDead[i]->setPos(40+SHIFT*j++,100+SHIFT*2*k);
+    }
+}
+
+void Game::displayDeadBlack()
+{
+    int SHIFT = 50;
+    int j = 0;
+    int k = 0;
+    for(size_t i = 0,n = blackDead.size(); i<n; i++) {
+        if(j == 4){
+            k++;
+            j = 0;
+        }
+        blackDead[i]->setPos(1140+SHIFT*j++,100+SHIFT*2*k);
+    }
+}
+
+void Game::placeInDeadPlace(ChessPiece *piece)
+{
+    if(piece->getSide() == "WHITE") {
+        whiteDead.append(piece);
+        King *g = dynamic_cast <King *>(piece);
+        if(g){
+
+            check->setPlainText("Black Won");
+            gameOver();
+        }
+        displayDeadWhite();
+    }
+    else{
+        blackDead.append(piece);
+        King *g = dynamic_cast <King *>(piece);
+        if(g){
+
+            check->setPlainText("White Won");
+            gameOver();
+        }
+        displayDeadBlack();
+    }
+    alivePiece.removeAll(piece);
+}
+
+void Game::addToScene(QGraphicsItem *item)
+{
+    gameScene->addItem(item);
+}
+
+void Game::removeFromScene(QGraphicsItem *item)
+{
+    gameScene->removeItem(item);
+
+}
+
+QString Game::getTurn()
+{
+    return turn;
+}
+
+void Game::setTurn(QString value)
+{
+    turn = value;
+}
+
+void Game::changeTurn()
+{
+    if(getTurn() == "WHITE")
+        setTurn("BLACK");
+    else
+        setTurn("WHITE");
+    turnDisplay->setPlainText("Turn : " + getTurn());
+}
+
+void Game::start()
+{
+    for(size_t i =0, n = listG.size(); i < n; i++)
+        removeFromScene(listG[i]);
+
+    addToScene(turnDisplay);
+    QGraphicsTextItem* whitePiece = new QGraphicsTextItem();
+    whitePiece->setPos(70,10);
+    whitePiece->setZValue(1);
+    whitePiece->setDefaultTextColor(Qt::white);
+    whitePiece->setFont(QFont("",14));
+    whitePiece->setPlainText("WHITE PIECE");
+    addToScene(whitePiece);
+    QGraphicsTextItem *blackPiece = new QGraphicsTextItem();
+
+    blackPiece->setPos(1170,10);
+    blackPiece->setZValue(1);
+    blackPiece->setDefaultTextColor(Qt::black);
+    blackPiece->setFont(QFont("",14));
+    blackPiece->setPlainText("BLACK PIECE");
+    addToScene(blackPiece);
+    addToScene(check);
+    chess->addChessPiece();
+}
+
+void Game::drawDeadHolder(int x, int y,QColor color)
+{
+    deadHolder = new QGraphicsRectItem(x,y,300,900);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(color);
+    deadHolder->setBrush(brush);
+    addToScene(deadHolder);
+}
+
+
+
+void Game::displayMainMenu()
+{
+    // Clear previous items
+    for (auto item : listG) {
+        removeFromScene(item);
+    }
+    listG.clear();
+
+    // Create background images and title text
+    QGraphicsPixmapItem *p = new QGraphicsPixmapItem();
+    p->setPixmap(QPixmap(":/images/king1.png"));
+    p->setPos(420,170);
+    addToScene(p);
+    listG.append(p);
+
+    QGraphicsPixmapItem *p1 = new QGraphicsPixmapItem();
+    p1->setPixmap(QPixmap(":/images/king.png"));
+    p1->setPos(920,170);
+    addToScene(p1);
+    listG.append(p1);
+
+    QGraphicsTextItem *titleText = new QGraphicsTextItem("Chess Pro");
+    QFont titleFont("arial" , 50);
+    titleText->setFont(titleFont);
+    int xPos = width()/2 - titleText->boundingRect().width()/2;
+    int yPos = 150;
+    titleText->setPos(xPos, yPos);
+    addToScene(titleText);
+    listG.append(titleText);
+
+    // Create username input
+    QLineEdit *usernameInput = new QLineEdit();
+    usernameInput->setPlaceholderText("Enter username");
+    usernameInput->setFixedWidth(200);
+    QGraphicsProxyWidget *proxyUsername = gameScene->addWidget(usernameInput);
+    proxyUsername->setPos(width()/2 - usernameInput->width()/2, 300);
+    listG.append(proxyUsername);
+
+    // Create password input
+    QLineEdit *passwordInput = new QLineEdit();
+    passwordInput->setPlaceholderText("Enter password");
+    passwordInput->setEchoMode(QLineEdit::Password); // Hide password input
+    passwordInput->setFixedWidth(200);
+    QGraphicsProxyWidget *proxyPassword = gameScene->addWidget(passwordInput);
+    proxyPassword->setPos(width()/2 - passwordInput->width()/2, 350);
+    listG.append(proxyPassword);
+
+    // Create login button
+    Button *loginButton = new Button("Login");
+    int btnXPos = width()/2 - loginButton->boundingRect().width()/2;
+    int btnYPos = 400;
+    loginButton->setPos(btnXPos, btnYPos);
+    connect(loginButton, &Button::clicked, this, [=]() {
+        QString username = usernameInput->text();
+        QString password = passwordInput->text();
+
+        if (username == "user" && password == "password") {
+            start();  // Start the game if credentials match
+        } else {
+            qDebug() << "Invalid credentials!";
+        }
+    });
+    addToScene(loginButton);
+    listG.append(loginButton);
+
+    drawChessBoard();
+}
+
+void Game::gameOver()
+{
+    //removeAll();
+    setTurn("WHITE");
+    alivePiece.clear();
+    chess->reset();
+
+
+
+
+}
+
+void Game::removeAll(){
+    QList<QGraphicsItem*> itemsList = gameScene->items();
+    for(size_t i = 0, n = itemsList.size();i<n;i++) {
+        if(itemsList[i]!=check)
+          removeFromScene(itemsList[i]);
+    }
+}
